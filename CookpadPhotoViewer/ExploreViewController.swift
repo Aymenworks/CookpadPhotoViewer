b8e550cbf1f7a655fed244632630d7b04414bcf7
Aymen Rebouh <aymen.rebouh@worldline.com>
2017-04-19 21:59:50 +0200

    Setup Cocoapods

---
+++ CookpadPhotoViewer/ExploreViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreViewController.swift
+//  CookpadPhotoViewer
+//
+//  Created by Rebouh Aymen on 19/04/2017.
+//  Copyright © 2017 Aymen. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Pods/Target Support Files/Pods-CookpadPhotoViewer/Pods-CookpadPhotoViewer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CookpadPhotoViewer : NSObject
+@end
+@implementation PodsDummy_Pods_CookpadPhotoViewer
+@end

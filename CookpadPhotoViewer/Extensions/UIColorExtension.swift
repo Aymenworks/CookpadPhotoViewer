09b02ab9e4eebbc9cd805f9d635d95afdae027e2
Aymen Rebouh <aymen.rebouh@worldline.com>
2017-04-20 21:12:52 +0200

    Add cookpad orange and brown colors

---
+++ CookpadPhotoViewer/Extensions/UIColorExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColorExtension.swift
+//  CookpadPhotoViewer
+//
+//  Created by Rebouh Aymen on 20/04/2017.
+//  Copyright © 2017 Aymen. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let cookpadBrown: UIColor = UIColor(red: 51/255, green: 0, blue: 0, alpha: 1.0)
+    static let cookpadOrange: UIColor = UIColor(red: 255/255, green: 153/255, blue: 51/255, alpha: 1.0)
+}

b9d3f1b1bc4e5b18aa2b41fd66937349421dedcb
Aymen Rebouh <aymen.rebouh@worldline.com>
2017-04-22 08:41:10 +0200

    Add unwind IBAction method in all controllers
    So I can easily connect buttons with dismiss controller actions from the storyboard

---
+++ CookpadPhotoViewer/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewControllerExtension.swift
+//  CookpadPhotoViewer
+//
+//  Created by Rebouh Aymen on 21/04/2017.
+//  Copyright © 2017 Aymen. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    @IBAction func unwind(segue: UIStoryboardSegue) {}
+}
